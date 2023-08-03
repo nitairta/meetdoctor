b60cdbaa834b6667858cb5bc0877b39d50368683
Nit <nitairtaa@gmail.com>
2023-08-04 00:30:05 +0700

    add foreign key and migrate detail user and role user

--- database/migrations/2023_08_03_075022_add_foreign_keys_to_detail_user_table.php
+++ database/migrations/2023_08_03_075022_add_foreign_keys_to_detail_user_table.php
@@ -14,7 +14,9 @@ class AddForeignKeysToDetailUserTable extends Migration
     public function up()
     {
         Schema::table('detail_user', function (Blueprint $table) {
-            //
+            
+            $table->foreign('user_id', 'fk_detail_user_to_users')->references('id')->on('users')->onUpdate('CASCADE')->onDelete('CASCADE');
+            $table->foreign('type_user_id', 'fk_detail_user_to_type_user')->references('id')->on('type_user')->onUpdate('CASCADE')->onDelete('CASCADE');
         });
     }
 
@@ -26,7 +28,9 @@ class AddForeignKeysToDetailUserTable extends Migration
     public function down()
     {
         Schema::table('detail_user', function (Blueprint $table) {
-            //
+            $table->dropForeign('fk_detail_user_to_users');
+            $table->dropForeign('fk_detail_user_to_type_user');
+       
         });
     }
 }
